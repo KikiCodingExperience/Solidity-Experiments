d25394d42fa27d02fbb3a978efad43f109745f7d
KikiCodingExperience <118337901+KikiCodingExperience@users.noreply.github.com>
2022-11-17 10:21:10 +0300

    Create Events.sol

---
+++ StakingContract/Events.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.13;
+
+contract Events {
+
+event staker(address indexed staker, uint256 stakingAmount);
+event unstaker(address indexed staker, uint256 unstakingAmount);
+event newAdmin(address indexed newAdmin);
+event newStakingToken(address indexed token);
+
+}
