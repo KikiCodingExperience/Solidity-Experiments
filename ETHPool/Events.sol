670cc1d1ca0d9a776419051e333ba9b6aa640461
KikiCodingExperience <118337901+KikiCodingExperience@users.noreply.github.com>
2022-11-18 17:16:14 +0300

    Update Events.sol

--- ETHPool/Events.sol
+++ ETHPool/Events.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity 0.8.13;
+
+contract Events {
+
+event newOwner(address indexed owner, address indexed newOwner);
+event deposit(address indexed depositer, uint256 amount);
+event withdraw(address indexed withdrawer, uint256 amount);
+event depositedRewards(address indexed owner, uint256 amount);
+
+}
