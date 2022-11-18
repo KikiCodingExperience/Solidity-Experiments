11fb77d2e0dc9656f3568e1ff5a64dd6bf32dc59
KikiCodingExperience <118337901+KikiCodingExperience@users.noreply.github.com>
2022-11-18 17:04:45 +0300

    Update CustomErrors.sol

--- ETHPool/CustomErrors.sol
+++ ETHPool/CustomErrors.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity 0.8.13;
+
+contract Errors {
+
+error NotOwner();
+error AddressZero();
+error SameOwner();
+error AmountZero();
+error NotDepositer();
+error InsufficientFunds();
+
+}
